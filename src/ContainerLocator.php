8a29c970a713c6603de479ba0597b88e86bdb6e6
Nigel Greenway <nigel_greenway@me.com>
2015-03-25 20:46:33 +0000

    Add ContainerLocator

---
+++ src/ContainerLocator.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace League\Tactician\Container;
+
+use League\Container\Container;
+use League\Tactician\Container\Exception\MissingCommandException;
+use League\Tactician\Container\Exception\MissingContainerServiceException;
+use League\Tactician\Command;
+use League\Container\Exception\ReflectionException;
+use League\Tactician\Handler\Locator\HandlerLocator;
+
+/**
+ * Fetch handler instances from an in-memory collection.
+ *
+ * This locator allows you to bind a handler fqcn to receive commands of a
+ * certain class name.
+ */
+class ContainerLocator implements HandlerLocator
+{
+    /**
+     * The Container object
+     *
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * The collection of Command/CommandHandler
+     *
+     * @var array
+     */
+    protected $commandToHandlerIdMap = [];
+
+    /**
+     * Class constructor
+     *
+     * @param Container $container                The Container object
+     * @param array     $commandClassToHandlerMap The Command/Handler mapping
+     */
+    public function __construct(
+        Container $container,
+        array $commandClassToHandlerMap = []
+    ) {
+        $this->container = $container;
+
+        if (empty($commandClassToHandlerMap) === false) {
+            $this->addHandlers($commandClassToHandlerMap);
+        }
+    }
+
+    /**
+     * Bind a handler instance to receive all commands with a certain class
+     *
+     * @param string $handlerId        Handler to receive class
+     * @param string $commandClassName Command class e.g. "My\TaskAddedCommand"
+     */
+    public function addHandler($handlerId, $commandClassName)
+    {
+        $this->commandToHandlerIdMap[$commandClassName] = $handlerId;
+    }
+
+    /**
+     * Allows you to add multiple handlers at once.
+     *
+     * The map should be an array in the format of:
+     *  [
+     *      'AddTaskCommand'      => 'AddTaskCommandHandler',
+     *      'CompleteTaskCommand' => 'CompleteTaskCommandHandler',
+     *  ]
+     *
+     * @param array $commandClassToHandlerMap The Command/Handler mapping
+     */
+    protected function addHandlers(array $commandClassToHandlerMap)
+    {
+        foreach ($commandClassToHandlerMap as $commandClass => $handler) {
+            $this->addHandler($handler, $commandClass);
+        }
+    }
+
+    /**
+     * Retrieve handler for the given command
+     *
+     * @param Command $command The command object
+     *
+     * @return Object
+     *
+     * @throws MissingHandlerException
+     */
+    public function getHandlerForCommand(Command $command)
+    {
+        $className = get_class($command);
+
+        if (!isset($this->commandToHandlerIdMap[$className])) {
+            throw MissingCommandException::forCommand($command);
+        }
+
+        $serviceId = $this->commandToHandlerIdMap[$className];
+
+        try {
+            return $this->container->get($serviceId);
+
+        } catch (ReflectionException $e) {
+            throw MissingContainerServiceException::forCommand($command);
+        }
+
+    }
+}
